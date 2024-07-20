94adcf49530475c9f6d3fad3ed1c25231db9076a
Om Prakash Sahoo <omprakashsahoo1234@gmail.com>
2024-07-20 20:52:55 +0530

    Implemented backend using partials ejs and covered rendering pages and static components of page

--- udemy-backend-learning/ejs/4.2/index.js
+++ udemy-backend-learning/ejs/4.2/index.js
@@ -4,11 +4,17 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {});
+app.get("/", (req, res) => {
 
-app.post("/submit", (req, res) => {});
+  res.render("index.ejs")
+});
+
+app.post("/submit", (req, res) => {
+
+  res.render("index.ejs", req.body)
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
