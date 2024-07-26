0c49d4b49fbadd4789cbf4a6cd327e84304c16c6
Om Prakash Sahoo <omprakashsahoo1234@gmail.com>
2024-07-26 15:10:31 +0530

    Encryption hashing and salting using bcrypt for authentication

--- udemy-backend-learning/authentication/level 1/index.js
+++ udemy-backend-learning/authentication/level 1/index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg"
+import bcyrpt from "bcrypt"
 
 const app = express();
 const port = 3000;
+const saltRounds = 10
 
 
 // creating a post gress client to create a database instance and connect to it 
@@ -36,21 +38,25 @@ app.get("/register", (req, res) => {
 app.post("/register", async (req, res) => {
   const email = req.body.username;
   const password = req.body.password;
-  // check to not register already existing user 
-  const checkList = await db.query("SELECT * FROM users WHERE email=$1", [email])
-  const emailExists = checkList.rows[0].email
 
-  if (emailExists) {
+  
+  const checkList = await db.query("SELECT * FROM users WHERE email=$1", [email])
+  console.log(email)
+
+  if (checkList.rows.length > 0) {
     res.render("secrets.ejs", {email:email})
 
   } else {
-    const result = await db.query("INSERT INTO users (email, password) VALUES ($1, $2)",[email, password])
-    res.render("secrets.ejs", {email:email})
+    bcyrpt.hash(password,saltRounds, async (err, hash) => {
+
+      const result = await db.query("INSERT INTO users (email, password) VALUES ($1, $2)",[email, hash])
+
+      res.render("secrets.ejs", {email:email})
+    })
 
   }
 
   // console.log(result)
-  // res.render("secrets.ejs")
 });
 
 app.post("/login", async (req, res) => {
@@ -58,14 +64,20 @@ app.post("/login", async (req, res) => {
   const password = req.body.password;
 
   const checkList = await db.query("SELECT * FROM users WHERE email=$1", [email])
-  const emailExists = checkList.rows[0].email
+  const emailExists = checkList.rows
 
   
-    if (emailExists && checkList.rows[0].password == password) {
+    if (emailExists.length > 0) {
+      const ismatch = await bcyrpt.compare(password, emailExists[0].password)
+      if (ismatch) {
+        res.render("secrets.ejs", {email:email})
 
-      res.render("secrets.ejs", {email:email})
+      } else {
+        res.render("login.ejs", {error:"Invalid credentials"})
+      }
+
     } else {
-      res.render("login.ejs", {error:"Not found"})
+      res.render("login.ejs", {error:"Invalid credentials"})
       // alert("No account found")
 
     }
